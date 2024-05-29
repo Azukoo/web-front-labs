3cb50e91fe1a3ae2c2717eb51d0c7a350d8aaa25
Виктория Болотовна <victoria.near@gmail.com>
2024-05-29 22:35:21 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -22,4 +22,23 @@ function showDate() {
     let out5 = document.getElementById('dates-out');
     let today5 = new Date();
     out5.innerHTML = "Дата и время для китайской локали : " + today5.toLocaleString('zh-SG');
+
+
+    let out6 = document.getElementById('current-year');
+    let today6 = new Date();
+    out6.innerHTML = "Текущий год: " + today6.getFullYear();
+
+    let out7 = document.getElementById('current-month');
+    let today7 = new Date();
+    let month = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+    out7.innerHTML = "Текущий месяц: " + month[today7.getMonth()];
+
+    let out8 = document.getElementById('current-day');
+    let today8 = new Date();
+    out8.innerHTML = "Текущая дата: " + today8.getDate();
+
+    let out9 = document.getElementById('current-weekday');
+    let today9 = new Date();
+    let days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+    out9.innerHTML = "День недели: " + days[today9.getDay()];
 }
