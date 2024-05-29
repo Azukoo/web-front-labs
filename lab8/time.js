63585cd52c2e66ee68a62d81d2042b74e29dbe73
Виктория Болотовна <victoria.near@gmail.com>
2024-05-30 01:18:46 +0700

    Аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -1,6 +1,20 @@
 function showTime() {
     let today = new Date();
-    let currentTime = today.toLocaleTimeString('ru-RU');
+    let currentTime = today.toLocaleTimeString('ru-Ru');
     document.getElementById('time').innerHTML = currentTime;
+};
+    setInterval(showTime, 1000);
+
+function updateSecondHand() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondHand = document.getElementById('secondHand');
+        
+    const rotationDegrees = seconds * 6; // Каждая секунда соответствует повороту на 6 градусов
+    secondHand.style.transform = `rotate(${rotationDegrees}deg)`;
 }
-setInterval(showTime, 1000);
+    
+    // Обновляем положение стрелки каждую секунду
+    setInterval(updateSecondHand, 1000);
+    
+    
